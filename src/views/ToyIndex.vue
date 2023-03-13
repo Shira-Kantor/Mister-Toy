6e78b178cbcabf09742b94c3da14f05c5af72c01
shira kantor <shira.shachar@gmail.com>
2023-03-13 16:43:16 +0200

    start

---
+++ src/views/ToyIndex.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <h1>
+        toy index
+    </h1>
+    {{ msg }}
+</template>
+
+<script>
+    import ToyList from '../components/ToyList.vue'
+export default {
+    
+ name: 'ToyIndex',
+    computed: {
+        msg() {
+            console.log(this.$store.getters.getMsg);
+            return this.$store.getters.getMsg
+        }
+    },
+}
+</script>
+
+<style></style>
